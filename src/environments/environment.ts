ef4af8606c328ab50e0e6232f6ec17692103a289
J Lomibao <jeanlomibao18@gmail.com>
2021-04-29 22:28:41 -0600

    prepare for deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  baseUrl: 'http://localhost:4200/'
+  production: false
 };
 
 /*
